253511b066a91b44c5f98869cfe0324c8a743860
MrThief123 <90836480+MrThief123@users.noreply.github.com>
2025-08-03 13:13:09 +1000

    fix: comments

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,15 +1,20 @@
-#!/bin/bash install dependanices. tomcat is Java application server 
-sudo yum install tomcat -y
-sudo yum -y install httpd
-sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
+#!/bin/bash
+
+# Install Tomcat
+sudo yum install -y tomcat
+
+# Install Apache HTTPD
+sudo yum install -y httpd
+
+# Configure Apache reverse proxy to Tomcat
+sudo bash -c 'cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
 <VirtualHost *:80>
   ServerAdmin root@localhost
   ServerName app.nextwork.com
   DefaultType text/html
-  ProxyRequests off
+  ProxyRequests Off
   ProxyPreserveHost On
   ProxyPass / http://localhost:8080/nextwork-web-project/
   ProxyPassReverse / http://localhost:8080/nextwork-web-project/
 </VirtualHost>
-EOF
-
+EOF'
